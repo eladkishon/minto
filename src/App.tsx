5382d5dfdeabd8e687f5360783d6cefe39795de6
eladkishon <elad.kishon@gmail.com>
2024-09-14 08:42:53 -0500

    complete base todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,84 @@
 import { useState } from 'react'
 import './App.css'
-import { Card, CardBody, Checkbox, Input } from '@nextui-org/react'
+import { Card, CardBody, Checkbox } from '@nextui-org/react'
 
-
-const NewToDo = () => {
-  return  <Input
-      classNames={{
-        label: "text-black/50 dark:text-white/90",
-        input: [
-          // "bg-transparent",
-          "text-black/90 dark:text-white/90",
-          "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-        ],
-
-      }}
-      placeholder="Type to search..."
-
-    />
+interface Todo {
+  text: string
+  done: boolean
 }
+
+const TodoList = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, { text: newTodo, done: false }]);
+      setNewTodo('');
+    }
+  };
+
+  const handleToggleCheckbox = (index: number) => {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, completed: !todo.done } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const handleEditTodo = (index: number, val: string) => {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, text: val } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-2'>
+        {todos.map((todo, index) => (
+          <div key={index}>
+            <Checkbox
+              isSelected={todo.done}
+              onChange={() => handleToggleCheckbox(index)}
+            >
+
+            </Checkbox>
+            <input
+              value={todo.text}
+              onChange={(e) => handleEditTodo(index, e.target.value)}
+              autoFocus
+            />
+
+          </div>
+        ))}
+      </div>
+      <input
+        placeholder="Add a task"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        onBlur={handleAddTodo}
+      />
+    </div>
+  );
+};
 
 function App() {
 
-  const [todos, setTodos] = useState(["hello", "esh"])
 
   return (
     <div className="flex flex-col items-center h-full">
       <div className='mt-20'>
-      <Card className='p-10'>
-        <CardBody >
+        <Card className='p-10'>
+          <CardBody >
 
-          <div className='flex flex-col gap-4'>
 
-            <div className='flex flex-col gap-2'>
-              {todos.map((todo) => {
-                return <div className='flex flex-row'>
-                  <Checkbox />
-                  <h1>{todo}</h1>
+            <TodoList />
 
-                </div>
-              })}
-            </div>
+          </CardBody>
+        </Card>
 
-            <div className='flex'>
-              <NewToDo />
-            </div>
-
-          </div>
-        </CardBody>
-      </Card>
 
       </div>
 
